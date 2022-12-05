daf8d1bd43ac88a3c78952470092210752a5e3dd
Florian Hoss <mail@florianhoss.de>
2022-12-05 13:58:18 +0100

    Add help

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -47,13 +47,38 @@ export default component$((props: { final: PropFunction<() => void> }) => {
               </svg>
             </button>
           )}
-          {store.showPrevious && (
+          {store.showPrevious ? (
             <button class="btn btn-secondary w-full sm:w-auto gap-2" onclick$={() => ctx.currentStep--}>
               <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">
                 <path d="M32 96C14.3 96 0 110.3 0 128s14.3 32 32 32l208 0V96L32 96zM192 288c-17.7 0-32 14.3-32 32s14.3 32 32 32h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm-64-64c0 17.7 14.3 32 32 32h48c17.7 0 32-14.3 32-32s-14.3-32-32-32H160c-17.7 0-32 14.3-32 32zm96 160c-17.7 0-32 14.3-32 32s14.3 32 32 32h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H224zm88-96l-.6 0c5.4 9.4 8.6 20.3 8.6 32c0 13.2-4 25.4-10.8 35.6c24.9 8.7 42.8 32.5 42.8 60.4c0 11.7-3.1 22.6-8.6 32H352c88.4 0 160-71.6 160-160V226.3c0-42.4-16.9-83.1-46.9-113.1l-11.6-11.6C429.5 77.5 396.9 64 363 64l-27 0c-35.3 0-64 28.7-64 64v88c0 22.1 17.9 40 40 40s40-17.9 40-40V160c0-8.8 7.2-16 16-16s16 7.2 16 16v56c0 39.8-32.2 72-72 72z" />
               </svg>
               Zurück
             </button>
+          ) : (
+            <>
+              <label for="docs" class="btn btn-secondary w-full sm:w-auto gap-2">
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">
+                  <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z" />
+                </svg>
+                Anleitung
+              </label>
+              <input type="checkbox" id="docs" className="modal-toggle" />
+              <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                  <h3 className="font-bold text-lg">Hallo, zufälliger Internetnutzer!</h3>
+                  <p className="font-normal pt-4">
+                    Das Quiz hat insgesamt 30 Fragen, aus denen beim Start 10 ausgewählt werden. Wenn alle Fragen richtig beantwortet wurden, gibt es am Ende
+                    ein kleines Geschenk.
+                  </p>
+                  <p className="font-normal py-2">Tipp: Der Schneefall kann ausgeschaltet werden.</p>
+                  <div className="modal-action">
+                    <label for="docs" className="btn btn-primary">
+                      Los gehts!
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </>
           )}
         </>
       ) : (
