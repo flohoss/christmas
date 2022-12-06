369e5a9b63a1068d09b47a3d1ba39ba5e68bb451
Florian Hoss <mail@florianhoss.de>
2022-12-06 09:54:42 +0100

    Add more code snippets

--- src/code/code.ts
+++ src/code/code.ts
@@ -6,6 +6,7 @@ export const CodeSnippets = [
   ["if (y != 0)", "\tx = y;", "else", "\tx = 0;"],
   ["if (a == 0)", "\ta = 0;"],
   ["function getRandomNumber()", "\treturn 43;"],
-  ['printf("Enter a smaller number ");', 'scanf("%d", a);', 'printf("Enter a bigger number ");', 'scanf("%d", b) ;', 'printf("Smaller number is %d", a) ;'],
+  ['printf("Enter a smaller number ");', 'scanf("%d", a);', 'printf("Enter a bigger number ");', 'scanf("%d", b);', 'printf("Smaller number is %d", a);'],
   ["try {", '\tscanf("%d", a);', "\tcallMethodA();", "} catch(Exception e) {", "\tcallMethodA();", "} finally {", "\tcallMethodA();", "}"],
+  ['["hip","hip"]', "/* hip hip array! */"],
 ];
